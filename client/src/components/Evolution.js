aadf458307438fa213f02460f34b053efa737411
Victor Garcia <victor.santos@sou.inteli.edu.br>
2025-07-23 07:10:48 -0300

    feat: addition of 100

--- client/src/components/Evolution.js
+++ client/src/components/Evolution.js
@@ -100,8 +100,10 @@ import img96 from './imgs/evolution/noventaseis.webp';
 import img97 from './imgs/evolution/noventasete.webp';
 import img98 from './imgs/evolution/noventaoito.webp';
 import img99 from './imgs/evolution/noventanove.webp';
+import video100 from './imgs/evolution/cem.mp4';
 
 const evolutionImages = [
+  video100,
   img99, img98, img97, img96, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10,
   img11, img12, img13, img14, img15, img16, img17, img18, img19, img20,
   img21, img22, img23, img24, img25, img26, img27, img28, img29, img30,
